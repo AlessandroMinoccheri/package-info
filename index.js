65a79e1eaa16ab5c88c610ff061fe7e6d3ee9273
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2014-09-10 21:36:11 +0200

    support custom registries

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 'use strict';
 var got = require('got');
-var endpoint = 'https://registry.npmjs.org/';
+var registryUrl = require('registry-url');
 
 module.exports = function (name, cb) {
-	got(endpoint + encodeURIComponent(name) + '/latest', function (err, data) {
-		if (err === 404) {
-			return cb(new Error('Package doesn\'t exist'));
-		}
-
-		if (err) {
-			return cb(err);
-		}
-
-		var data_parsed = JSON.parse(data);
-		var name = data_parsed.name;
-		var version = data_parsed.version;
-		var description = data_parsed.description;
-		var license = data_parsed.license;
-		var homepage = data_parsed.homepage;
-		var author_name = data_parsed.author.name;
-
-		cb(null, 'name: ' + name + ' \nversion: ' + version + ' \ndescription: ' + description + ' \nlicense: ' + license + ' \nhomepage: ' + homepage + ' \nauthor: ' + author_name);
+	registryUrl(function (err, url) {
+		got(url + encodeURIComponent(name), {method: 'HEAD'}, function (err) {
+		    if (err === 404) {
+		    	return cb(new Error('Package doesn\'t exist'));
+		    }
+    
+		    if (err) {
+		    	return cb(err);
+		    }
+    
+		    var data_parsed = JSON.parse(data);
+		    var name = data_parsed.name;
+		    var version = data_parsed.version;
+		    var description = data_parsed.description;
+		    var license = data_parsed.license;
+		    var homepage = data_parsed.homepage;
+		    var author_name = data_parsed.author.name;
+    
+		    cb(null, 'name: ' + name + ' \nversion: ' + version + ' \ndescription: ' + description + ' \nlicense: ' + license + ' \nhomepage: ' + homepage + ' \nauthor: ' + author_name);
+		});
 	});
 };
