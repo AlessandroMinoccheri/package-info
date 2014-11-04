fb078d9d5063a2e8c2eb1ed50ca98e54b030f9e6
Krzysztof Kaczor <krzkaczor@gmail.com>
2014-11-04 15:16:47 +0100

    fixes - now CLI is working correctly

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ var registryUrl = require('registry-url');
 
 module.exports = function (name, cb) {
 	registryUrl(function (err, url) {
-		got(url + encodeURIComponent(name), {method: 'HEAD'}, function (err) {
+		got(url + encodeURIComponent(name), {method: 'GET'}, function (err, data) {
 		    if (err === 404) {
 		    	return cb(new Error('Package doesn\'t exist'));
 		    }
-    
+
 		    if (err) {
 		    	return cb(err);
 		    }
-    
+
 		    var data_parsed = JSON.parse(data);
 		    var name = data_parsed.name;
 		    var version = data_parsed.version;
@@ -20,7 +20,7 @@ module.exports = function (name, cb) {
 		    var license = data_parsed.license;
 		    var homepage = data_parsed.homepage;
 		    var author_name = data_parsed.author.name;
-    
+
 		    cb(null, 'name: ' + name + ' \nversion: ' + version + ' \ndescription: ' + description + ' \nlicense: ' + license + ' \nhomepage: ' + homepage + ' \nauthor: ' + author_name);
 		});
 	});
