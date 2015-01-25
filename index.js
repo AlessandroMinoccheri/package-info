e116d2d265f3f53922b6af101ff8d6606091ea43
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2015-01-25 10:58:51 +0100

    fixed bug if author is empty check

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var registryUrl = require('registry-url');
 
 module.exports = function (name, cb) {
 	got(registryUrl + encodeURIComponent(name), {method: 'GET'}, function (err, data) {
+		var name 		= '';
+		var version 	= '';
+		var description = '';
+		var license 	= '';
+		var homepage 	= '';
+		var author_name = '';
+
 		if (err === 404) {
 			return cb(new Error('Package doesn\'t exist'));
 		}
@@ -13,12 +20,31 @@ module.exports = function (name, cb) {
 		}
 
 		var data_parsed = JSON.parse(data);
-		var name = data_parsed.name;
-		var version = data_parsed[ 'dist-tags' ].latest;
+		var name 		= data_parsed.name;
+		var version 	= data_parsed[ 'dist-tags' ].latest;
 		var description = data_parsed.description;
-		var license = data_parsed.license;
-		var homepage = data_parsed.homepage;
-		var author_name = data_parsed.author.name;
+		var license 	= data_parsed.license;
+		
+		if(data_parsed.homepage !== undefined){
+			var homepage 	= data_parsed.homepage;
+		}
+
+		if(data_parsed.author !== undefined){
+			var author_name = data_parsed.author.name;
+		}
+		else{
+			if(data_parsed.maintainers !== undefined){
+				for (var i in data_parsed.maintainers) {
+					var maintainer = data_parsed.maintainers[i];
+					if(author_name == ''){
+						author_name = maintainer.name;
+					}
+					else{
+						author_name = author_name + ', ' + maintainer.name;
+					}
+				}
+			}
+		}
 
 		cb(null, 'name: ' + name + ' \nversion: ' + version + ' \ndescription: ' + description + ' \nlicense: ' + license + ' \nhomepage: ' + homepage + ' \nauthor: ' + author_name);
 	});
