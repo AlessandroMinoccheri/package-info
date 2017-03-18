ec65d3a60b9f1fd622b303dda81c48493b70d69d
Alessandro Minoccheri <alessandro@studiomado.it>
2017-03-18 18:44:50 +0100

    new manage with promise

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 'use strict';
 var got = require('got');
 var registryUrl = require('registry-url');
+var Promise = require('pinkie-promise');
+
+module.exports = function (name) {
+	if (typeof name !== 'string') {
+		return Promise.reject(new Error('username required'));
+	}
+
+	return got.head(registryUrl + name.toLowerCase())
+		.then(function (res) {
+			return 'test';
+		})
+		.catch(function (err) {
+			if (err.statusCode === 404) {
+				err.message = 'Package doesn\'t exist';
+			}
+
+			throw err;
+		});
+
+	/*return got(url).then(function (res) {
+		var $ = cheerio.load(res.body);
+
+		return {
+			name: $('.fullname').text() || null,
+			email: unobfuscateEmail($('.email [data-email]').attr('data-email')) || null,
+			homepage: $('.homepage a').attr('href') || null,
+			github: $('.github a').text().slice(1) || null,
+			twitter: $('.twitter a').text().slice(1) || null,
+			freenode: $('.freenode a').text() || null
+		};
+	}).catch(function (err) {
+		if (err.statusCode === 404) {
+			err.message = 'User doesn\'t exist';
+		}
+
+		throw err;
+	});*/
+};
 
 /*function request(name) {
 	return got.head(registryUrl + name.toLowerCase())
@@ -13,7 +51,7 @@ var registryUrl = require('registry-url');
 			throw err;
 		});
 }
-
+/*
 module.exports = name => {
 	if (!(typeof name === 'string' && name.length !== 0)) {
 		return Promise.reject(new Error('Package name required'));
@@ -22,7 +60,13 @@ module.exports = name => {
 	return request(name);
 };*/
 
-module.exports = function (name, cb) {
+/*module.exports = name => {
+	//if (!(typeof name === 'string' && name.length !== 0)) {
+	//	return Promise.reject(new Error('Package name required'));
+	//}
+
+	return request(name);
+	/*
 	got(registryUrl + encodeURIComponent(name), {method: 'GET'}, function (err, data) {
 		var name 		= '';
 		var version 	= '';
@@ -74,5 +118,5 @@ module.exports = function (name, cb) {
 			homepage		: homepage,
 			author			: author_name
 		});
-	});
-};
+	});*/
+//};
