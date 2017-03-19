685ab63478edec6fc9bffcc6cc757dc31e9f353f
Alessandro Minoccheri <alessandro@studiomado.it>
2017-03-19 12:10:16 +0100

    start implements new tests

--- test.js
+++ test.js
@@ -1,4 +1,27 @@
-'use strict';
+import test from 'ava';
+import m from '.';
+
+function randomName() {
+	return 'asdasfgrgafadsgaf${Math.random().toString().slice(2)}';
+}
+
+test('returns true when package name is available', async t => {
+	t.true(await m(randomName()));
+});
+
+test('returns false when package name is taken', async t => {
+	t.false(await m('chalk'));
+});
+
+test('returns a map of multiple package names', async t => {
+	const name1 = 'chalk';
+	const name2 = randomName();
+	const res = await m.many([name1, name2]);
+	t.false(res.get(name1));
+	t.true(res.get(name2));
+});
+
+/*'use strict';
 var assert = require('assert');
 var info = require('./index');
 
@@ -13,3 +36,4 @@ it('should get the latest info of a package', function (cb) {
 		cb();
 	});
 });
+*/
