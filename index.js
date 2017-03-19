eaeb6e1570cfda5695649facf80bfb42776b07de
Alessandro Minoccheri <alessandro@studiomado.it>
2017-03-19 11:36:11 +0100

    fixed script dependency

--- index.js
+++ index.js
@@ -5,12 +5,53 @@ var Promise = require('pinkie-promise');
 
 module.exports = function (name) {
 	if (typeof name !== 'string') {
-		return Promise.reject(new Error('username required'));
+		return Promise.reject(new Error('package name required'));
 	}
 
 	return got.head(registryUrl + name.toLowerCase())
-		.then(function (res) {
-			return 'test';
+		.then(function (data) {
+			var name 		= '';
+			var version 	= '';
+			var description = '';
+			var license 	= '';
+			var homepage 	= '';
+			var authorName = '';
+
+			var dataParsed = JSON.parse(data);
+			name 		= dataParsed.name;
+			version 	= dataParsed[ 'dist-tags' ].latest;
+			description = dataParsed.description;
+			license 	= dataParsed.license;
+
+			if(dataParsed.homepage !== undefined){
+				homepage 	= dataParsed.homepage;
+			}
+
+			if(dataParsed.author !== undefined){
+				authorName = dataParsed.author.name;
+			}
+			else{
+				if(dataParsed.maintainers !== undefined){
+					for (var i in dataParsed.maintainers) {
+						var maintainer = dataParsed.maintainers[i];
+						if(authorName === ''){
+							authorName = maintainer.name;
+						}
+						else{
+							authorName = authorName + ', ' + maintainer.name;
+						}
+					}
+				}
+			}
+
+			return {
+				name          	: name,
+				version			: version,
+				description		: description,
+				license			: license,
+				homepage		: homepage,
+				author			: authorName
+			};
 		})
 		.catch(function (err) {
 			if (err.statusCode === 404) {
